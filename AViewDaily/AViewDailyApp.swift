2fb67c54a68280a66d7ff54c40ddc7ae37cf4777
LMuchapirei <muchapireil@gmail.com>
2024-05-03 12:25:46 +0200

    Initial Commit

---
+++ AViewDaily/AViewDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AViewDailyApp.swift
+//  AViewDaily
+//
+//  Created by Linval Muchapirei on 3/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct AViewDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
