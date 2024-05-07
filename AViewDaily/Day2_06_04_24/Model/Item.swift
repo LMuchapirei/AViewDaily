bf9a1b3395c8a340845b4a835bf4ed47cf042ffe
LMuchapirei <muchapireil@gmail.com>
2024-05-07 09:58:43 +0200

    Add sample data based on the model

--- AViewDaily/Day2_06_04_24/Model/Item.swift
+++ AViewDaily/Day2_06_04_24/Model/Item.swift
@@ -19,3 +19,30 @@ struct Item: Identifiable,Hashable {
     var image: UIImage?
     
 }
+
+
+
+var sampleImages: [Item] = [
+// Image Link: https://www.pexels.com/photo/river-and-lake-behind-18970192/
+    .init(title: "Abril Altamirano", image: UIImage(named: "Pic 1")),
+// Image Link: https://www.pexels.com/photo/hot-air-balloons-flying-in-cappadocia-at-sunrise-18873058/
+    .init(title: "Gülsah Aydogan", image: UIImage(named: "Pic 2" )),
+// Image Link: https://www.pexels.com/photo/winter-landscape-with-wooden-cottage-in-snowy-forest-8001019/
+    .init(title: "Melike Sayar Melikesayar", image: UIImage (named: "Pic 3")),
+// Image Link: https://www.pexels.com/photo/branches-with-green-leaves-in-nature-8058776/
+    .init(title: "Maahid Photos", image: UIImage(named: "Pic 4")),
+// Image Link: https://www.pexels.com/photo/bush-with-green-fern-leaves-7945985/
+    .init(title: "Pelageia Zelenina", image: UIImage (named: "Pic 5")),
+/// Image Link: https://www.pexels.com/photo/tree-growing-among-agricultural-field-in-countryside-7939089/
+    .init(title: "Ofir Eliav", image: UIImage(named: "Pic 6")),
+/// Image Link: https://www.pexels.com/photo/scenic-landscape-of-autumn-forest-with-tall-trees-in-fog-8001169/
+    .init(title: "Melike Sayar Melikesayar", image: UIImage(named: "Pic 7")),
+/// Image Link: https://www.pexels.com/photo/winter-forest-with-river-and-wooden-footbridge-8000786/
+    .init(title: "Melike Sayar Melikesayar", image: UIImage(named: "Pic 8")),
+//// Image Link: https://www.pexels.com/photo/old-city-with-illuminated-buildings-and-roads-at-night-8000995/
+    .init(title: "Melike Sayar Melikesayar", image: UIImage(named: "Pic 9")),
+/// Image Link: https://www.pexels.com/photo/shore-with-dried-grass-near-river-7724075/
+    .init(title: "Erik Mclean", image: UIImage(named: "Pic 10" )),
+/// Image Link: https://www.pexels.com/photo/person-hand-with-bouquet-of-flowers-on-narrow-footbridge-9387799/
+    .init(title: "Fatma DELIASLAN", image: UIImage(named: "Pic 11")),
+    ]
