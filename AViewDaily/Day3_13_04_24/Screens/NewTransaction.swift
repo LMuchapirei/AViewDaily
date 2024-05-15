2fa3b78c1ad7bbe763e63bb0d5a557143756a52d
LMuchapirei <muchapireil@gmail.com>
2024-05-15 15:15:37 +0200

    View to select transaction options

--- AViewDaily/Day3_13_04_24/Screens/NewTransaction.swift
+++ AViewDaily/Day3_13_04_24/Screens/NewTransaction.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct NewTransaction: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment:.leading,spacing: 15){
+            Button(action:{
+                dismiss()
+            }) {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.title)
+                    .tint(.red)
+            }
+            .hSpacing(.leading)
+            .padding([.top,.horizontal],5)
+            
+            VStack(alignment: .leading,spacing: 8, content: {
+                Divider()
+                    .padding(.horizontal,100)
+                Text("Add New Transaction")
+                    .font(.title2)
+                    .foregroundStyle(.gray)
+                    .padding(.horizontal,80)
+                    .padding(.vertical,10)
+                
+                    ForEach(TransactionType.allCases,id:\.self){ item in
+                        VStack {
+                            HStack {
+                                item.leadingIcon
+                                Text(item.rawValue)
+                                Spacer()
+                                Image(systemName: "arrow.right")
+                            }
+                            Divider()
+                        }
+                        .padding(.horizontal)
+                        .padding(.vertical,8)
+                        
+                    }
+
+            })
+            .padding(.top,5)
+
+        }
     }
 }
 
 #Preview {
-    NewTransaction()
+    ContentView()
 }
