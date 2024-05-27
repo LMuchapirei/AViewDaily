a4ee3d0bf5d4417d5ff75c5055e2874cea18631c
LMuchapirei <muchapireil@gmail.com>
2024-05-27 14:58:01 +0200

    Add tabs

--- AViewDaily/Day4_27_05_24/ScrollableTabs.swift
+++ AViewDaily/Day4_27_05_24/ScrollableTabs.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct ScrollableTabs: View {
+    @State private var activeTab: DummyTab = .home
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:15) {
+            TabView(selection:$activeTab){
+                DummyTab.home.color
+                    .tag(DummyTab.home)
+                
+                DummyTab.charts.color
+                    .tag(DummyTab.charts)
+                
+                DummyTab.calls.color
+                    .tag(DummyTab.calls)
+                
+                DummyTab.settings.color
+                    .tag(DummyTab.settings)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+        }
     }
 }
 
 #Preview {
     ScrollableTabs()
 }
+
+
+/// SwiftUI Page TabView is built on top of the UIKit's UICollectionView which in turn provides the content offset so we can extract the UIKit's UICollectionView from the SwiftUI Page Tab View
