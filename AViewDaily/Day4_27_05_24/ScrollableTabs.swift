763e97c582ed2d708d5990ca9ee5fbc39c66665e
LMuchapirei <muchapireil@gmail.com>
2024-05-27 15:14:46 +0200

    Extract the underlying CollectionView

--- AViewDaily/Day4_27_05_24/ScrollableTabs.swift
+++ AViewDaily/Day4_27_05_24/ScrollableTabs.swift
@@ -15,6 +15,9 @@ struct ScrollableTabs: View {
             TabView(selection:$activeTab){
                 DummyTab.home.color
                     .tag(DummyTab.home)
+                    .background {
+                        FindCollectionView()
+                    }
                 
                 DummyTab.charts.color
                     .tag(DummyTab.charts)
@@ -36,3 +39,53 @@ struct ScrollableTabs: View {
 
 
 /// SwiftUI Page TabView is built on top of the UIKit's UICollectionView which in turn provides the content offset so we can extract the UIKit's UICollectionView from the SwiftUI Page Tab View
+///
+///
+
+
+/// on iOS 16 simply change this to conform to ObservableObject
+
+@Observable
+class PageOffsetObserver: NSObject {
+    var collectionView: UICollectionView?
+    var offset: CGFloat = 0
+    private (set) var isObserving: Bool = false
+    
+    func observe(){
+        
+    }
+    
+    func remove(){
+        
+    }
+}
+
+struct FindCollectionView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()){
+            if let collectionView = view.collectionSuperView {
+                print(collectionView)
+            }
+        }
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+}
+
+/// how to find the view we want by traversing the superview
+///
+
+extension UIView {
+    var collectionSuperView: UICollectionView? {
+        if let collectionView = superview as? UICollectionView {
+            return collectionView
+        }
+        return superview?.collectionSuperView
+    }
+}
